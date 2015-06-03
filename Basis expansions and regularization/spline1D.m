8da987b7d061b152a556fc960196bf57f23d9e8c
rmasciar <roberto.masciarelli@hotmail.it>
2015-06-03 16:52:21 +0200

    Update spline1D.m
    replaced content with file in linear regression methods, this is just for completeness

--- Basis expansions and regularization/spline1D.m
+++ Basis expansions and regularization/spline1D.m
@@ -1,81 +1,103 @@
-function [h knot]=spline1D(x,type,varargin)
+function splines=splines1D(x,type,options={},epsilon)
 
-  for i=2:2:numel(varargin) 
-   eval(strcat(varargin{(i-1)}, '=', varargin{i},';'));
+  # spline methods implemented:
+  # - general spline 
+  # - natural cubic spline 
+  # - b spline
+  
+  # possible to change M (order of spline), knots (internal knots of the domain), nK (number of knots) 
+  # 
+
+  for i=2:2:numel(options) 
+    eval(strcat(options{(i-1)}, '=[', num2str(options{i}(:)'),'];'))
   end
-
+  
   x=x(:);
-  m=length(x);
+  m=numel(x);
+  
+  if ~exist('epsilon', 'var') || isempty(epsilon)
+    epsilon=[min(x); max(x)];
+  end
 
   if ~exist('M', 'var') || isempty(M)
     M=4;
   end
-
-  if ~exist('K', 'var') || isempty(K)
-    knot=unique(x)(2:(end-1));
-    K=length(knot);
+    
+  if ~exist('knots', 'var') || isempty(knots)   
+    if ~exist('nK', 'var') || isempty(nK)
+      knots=unique(x)(2:(end-1))(:);
+      nK=numel(knots);
+    else
+      knots=linspace(epsilon(1),epsilon(2),nK+2)(2:(end-1))(:);
+    end
   else
-    knot=linspace(min(x),max(x),K+2)(2:(end-1))';
-    K=length(knot);
+    nK=numel(knots);
   end
 
   switch type
-    case 'general'
-      h=zeros(m,M+K);
+  
+  # general spline
+    case 'general spline'     
+      h=zeros(m,M+nK);
       for j=1:M
         h(:,j)=x.^(j-1);
       end
-      for l=1:K
-        h(:,M+l)=(x>=knot(l)).*(x-knot(l)).^(M-1);
+      for l=1:nK
+        h(:,M+l)=(x>=knots(l)).*(x-knots(l)).^(M-1);
       end
 
-    case 'natural cubic'
-     h=zeros(m,K);
-     h(:,1)=1;
-     h(:,2)=x;
-     k=K-1;
-     dK_1=(((x>=knot(k)).*(x-knot(k))).^3-((x>=knot(K)).*(x-knot(K))).^3)./(knot(K)-knot(k));
-     for k=1:(K-2)
-       dk=(((x>=knot(k)).*(x-knot(k))).^3-((x>=knot(K)).*(x-knot(K))).^3)./(knot(K)-knot(k));
-       h(:,k+2)=dk-dK_1;
-     end
-
+  # natural cubic spline
+    case 'natural cubic spline'     
+      h=zeros(m,nK);
+      h(:,1)=1;
+      h(:,2)=x;
+      k=nK-1;
+      dK_1=(((x>=knots(k)).*(x-knots(k))).^3-((x>=knots(nK)).*(x-knots(nK))).^3)./(knots(nK)-knots(k));
+      for k=1:(nK-2)
+        dk=(((x>=knots(k)).*(x-knots(k))).^3-((x>=knots(nK)).*(x-knots(nK))).^3)./(knots(nK)-knots(k));
+        h(:,k+2)=dk-dK_1;
+      end  
+      
+  # b spline
     case 'b spline'
-knot
-      tau=[repmat(min(x),M,1);knot;repmat(max(x),M,1)]
-#tau=[linspace(min(x)-0.0001,min(x),M)';knot;linspace(max(x),max(x)+0.0001,M)'];
-      for m_=1:M
-        b=zeros(m,K+2*M-m_);
-
-        for i=1:(K+2*M-m_)
-          if m_==1
-if tau(i)==tau(i+1)
-b(:,i)=0;
-else
-            b(:,i)=(x>=tau(i))&(x<tau(i+1));
-end
-          else
-if tau(i)==tau(i+m_-1)==tau(i+m_)==tau(i+1)
-b(:,i)=0;
-else
-            b(:,i)=b_prev(:,i).*(x-tau(i))/(tau(i+m_-1)-tau(i))+...
-              b_prev(:,i+1).*(tau(i+m_)-x)/(tau(i+m_)-tau(i+1));
-end    
+      #tau=[repmat(min(x),M,1);knots(:);repmat(max(x),M,1)];
+      delta=10^5;
+      step=delta/max(1,(M-1));
+      if M==1
+        tau=[epsilon(1);knots(:);epsilon(2)];
+      else
+        tau=[((epsilon(1)-delta):step:epsilon(1))';knots(:);(epsilon(2):step:(epsilon(2)+delta))'];
       end
+      for m_=1:M
+        b=zeros(m,nK+2*M-m_);
+        for i=1:(nK+2*M-m_)
+          if m_==1
+            #if tau(i)==tau(i+1)
+            #  b(:,i)=0;
+            #else
+              b(:,i)=(x>=tau(i))&(x<tau(i+1));
+            #end
+          else
+           # if tau(i)==tau(i+m_-1)||tau(i+m_)==tau(i+1)
+           #   b(:,i)=0;
+          #  else
+              b(:,i)=b_prev(:,i).*(x-tau(i))/(tau(i+m_-1)-tau(i))+...
+                b_prev(:,i+1).*(tau(i+m_)-x)/(tau(i+m_)-tau(i+1));
+            #end    
+          end
         end
         b_prev=b;
       end
+      splines.tau=tau;
       h=b;
- 
+  
   endswitch
-
-  if exist('y', 'var') && ~isempty(y)
-
-    f=h*pinv(h'*h)*h'*y;
-    plot(x,y,'*')
-    hold on
-    plot(x,f,'-')
-    hold off
-  end
-
+  
+    splines.epsilon=epsilon;
+    splines.M=M;
+    splines.nK=nK;
+    splines.knots=knots;
+    splines.h=h;
+    
+  
 end
