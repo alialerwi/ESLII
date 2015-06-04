b80ade580fd995191333c35e3c4a118c70591843
rmasciar <roberto.masciarelli@hotmail.it>
2015-06-04 15:01:32 +0200

    Update splines1D.m
    smoothing spline added

--- Linear methods for regression/splines1D.m
+++ Linear methods for regression/splines1D.m
@@ -4,8 +4,9 @@ function splines=splines1D(x,type,options={},epsilon)
   # - general spline 
   # - natural cubic spline 
   # - b spline
+  # - smoothing spline
   
-  # possible to change M (order of spline), knots (internal knots of the domain), nK (number of knots) 
+  # possible to change M (order of spline), knots (internal knots of the domain), nK (number of knots), lambda 
   # 
 
   for i=2:2:numel(options) 
@@ -70,12 +71,16 @@ function splines=splines1D(x,type,options={},epsilon)
       end
       for m_=1:M
         b=zeros(m,nK+2*M-m_);
+        db=zeros(m,nK+2*M-m_);
+        ddb=zeros(m,nK+2*M-m_);
         for i=1:(nK+2*M-m_)
           if m_==1
             #if tau(i)==tau(i+1)
             #  b(:,i)=0;
             #else
               b(:,i)=(x>=tau(i))&(x<tau(i+1));
+              db(:,i)=0;
+              ddb(:,i)=0;
             #end
           else
            # if tau(i)==tau(i+m_-1)||tau(i+m_)==tau(i+1)
@@ -83,13 +88,83 @@ function splines=splines1D(x,type,options={},epsilon)
           #  else
               b(:,i)=b_prev(:,i).*(x-tau(i))/(tau(i+m_-1)-tau(i))+...
                 b_prev(:,i+1).*(tau(i+m_)-x)/(tau(i+m_)-tau(i+1));
+              db(:,i)=db_prev(:,i).*(x-tau(i))/(tau(i+m_-1)-tau(i))+...
+                b_prev(:,i)./(tau(i+m_-1)-tau(i))+...
+                db_prev(:,i+1).*(tau(i+m_)-x)/(tau(i+m_)-tau(i+1))+...
+                b_prev(:,i+1).*(-1)/(tau(i+m_)-tau(i+1));
+              ddb(:,i)=ddb_prev(:,i).*(x-tau(i))/(tau(i+m_-1)-tau(i))+...
+                db_prev(:,i)./(tau(i+m_-1)-tau(i))+...
+                db_prev(:,i)./(tau(i+m_-1)-tau(i))+...
+                ddb_prev(:,i+1).*(tau(i+m_)-x)/(tau(i+m_)-tau(i+1))+...
+                db_prev(:,i+1).*(-1)/(tau(i+m_)-tau(i+1))+...
+                db_prev(:,i+1).*(-1)/(tau(i+m_)-tau(i+1));
             #end    
           end
         end
         b_prev=b;
+        db_prev=db;
+        ddb_prev=ddb;
       end
       splines.tau=tau;
       h=b;
+      
+  # smoothing spline
+    case 'smoothing spline'
+      #tau=[repmat(min(x),M,1);knots(:);repmat(max(x),M,1)];
+      delta=10^5;
+      step=delta/max(1,(M-1));
+      if M==1
+        tau=[epsilon(1);knots(:);epsilon(2)];
+      else
+        tau=[((epsilon(1)-delta):step:epsilon(1))';knots(:);(epsilon(2):step:(epsilon(2)+delta))'];
+      end
+      for m_=1:M
+        b=zeros(m,nK+2*M-m_);
+        db=zeros(m,nK+2*M-m_);
+        ddb=zeros(m,nK+2*M-m_);
+        for i=1:(nK+2*M-m_)
+          if m_==1
+            #if tau(i)==tau(i+1)
+            #  b(:,i)=0;
+            #else
+              b(:,i)=(x>=tau(i))&(x<tau(i+1));
+              db(:,i)=0;
+              ddb(:,i)=0;
+            #end
+          else
+           # if tau(i)==tau(i+m_-1)||tau(i+m_)==tau(i+1)
+           #   b(:,i)=0;
+          #  else
+              b(:,i)=b_prev(:,i).*(x-tau(i))/(tau(i+m_-1)-tau(i))+...
+                b_prev(:,i+1).*(tau(i+m_)-x)/(tau(i+m_)-tau(i+1));
+              db(:,i)=db_prev(:,i).*(x-tau(i))/(tau(i+m_-1)-tau(i))+...
+                b_prev(:,i)./(tau(i+m_-1)-tau(i))+...
+                db_prev(:,i+1).*(tau(i+m_)-x)/(tau(i+m_)-tau(i+1))+...
+                b_prev(:,i+1).*(-1)/(tau(i+m_)-tau(i+1));
+              ddb(:,i)=ddb_prev(:,i).*(x-tau(i))/(tau(i+m_-1)-tau(i))+...
+                db_prev(:,i)./(tau(i+m_-1)-tau(i))+...
+                db_prev(:,i)./(tau(i+m_-1)-tau(i))+...
+                ddb_prev(:,i+1).*(tau(i+m_)-x)/(tau(i+m_)-tau(i+1))+...
+                db_prev(:,i+1).*(-1)/(tau(i+m_)-tau(i+1))+...
+                db_prev(:,i+1).*(-1)/(tau(i+m_)-tau(i+1));
+            #end    
+          end
+        end
+        b_prev=b;
+        db_prev=db;
+        ddb_prev=ddb;
+      end
+      h=b;
+      omega=zeros(size(h,2));
+      for j=1:size(h,2)
+        for k=1:size(h,2)
+          omega(j,k)=sum(ddb(:,j).*ddb(:,k));
+        end
+      end
+      
+      splines.tau=tau;
+      splines.lambda=lambda;
+      splines.omega=omega;
   
   endswitch
   
@@ -98,6 +173,5 @@ function splines=splines1D(x,type,options={},epsilon)
     splines.nK=nK;
     splines.knots=knots;
     splines.h=h;
-    
   
 end
